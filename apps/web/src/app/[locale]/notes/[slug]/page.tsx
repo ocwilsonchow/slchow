4e28eb660e63756e441fd0d7b673a87c46cd1eb1
Wilson Chow <wilson.chow@vioo.com.hk>
2026-08-06 17:51:57 +0800

    fix(notes): improve layout and scrolling behavior in note page
    - Updated the Header.Column component to include max height and overflow properties, enhancing the user experience by allowing for better scrolling within the notes page.
    - Added a data attribute to prevent Lenis scrolling behavior, ensuring smoother navigation.

--- apps/web/src/app/[locale]/notes/[slug]/page.tsx
+++ apps/web/src/app/[locale]/notes/[slug]/page.tsx
@@ -40,7 +40,10 @@ const Page = async ({ params }: Props) => {
         <Header.Column>
           <BackLink href="/notes" />
         </Header.Column>
-        <Header.Column className="grid gap-5">
+        <Header.Column
+          className="grid gap-5 max-h-screen overflow-y-auto pb-20"
+          data-lenis-prevent
+        >
           <div className="mt-8 lg:mt-0 space-y-1">
             <h1 className="text-lg lg:text-sm font-semibold tracking-tight text-content-ink">
               {page?.data.title}
