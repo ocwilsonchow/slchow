d07d4c578dcd5c9a2292d554335d91e40ced3f3c
Wilson Chow <wilson.chow@vioo.com.hk>
2026-08-06 13:32:35 +0800

    feat(env): add USERNAME and PASSWORD secrets to SST environment definitions
    - Introduced new secrets for USERNAME and PASSWORD in the SST environment type definitions to enhance security for sensitive information.
    - Updated the build script in package.json to include fumadocs-mdx for improved documentation generation.
    - Made minor adjustments to the web app's SST environment declaration file for consistency.

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -6,6 +6,14 @@
 
 declare module "sst" {
   export interface Resource {
+    "PASSWORD": {
+      "type": "sst.sst.Secret"
+      "value": string
+    }
+    "USERNAME": {
+      "type": "sst.sst.Secret"
+      "value": string
+    }
     "WEB": {
       "type": "sst.aws.Nextjs"
       "url": string
